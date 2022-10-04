7e12f3d5f3df48078a1d805e80d04344735d3919
Ridwan Hussein <ridwan.hussein@kcl.ac.uk>
2022-10-04 15:04:21 +0100

    Changed the code to raise exception if 0 or a negative number is input

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f'Number of primes is {number_of_primes} but it has to be positive')
     list = []
+    i=1
+    while len(list) < number_of_primes:
+        factorList = []
+        for n in range(1,i+1):
+            if i%n == 0:
+                factorList.append(n)
+            elif len(factorList) > 2:
+                break
+        if len(factorList) == 2:
+            list.append(i)
+        i += 1
     return list
+
+    
+print(primes(0))
